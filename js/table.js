48887b338ead4feae82a0378c8c20fe1e01eb96c
YZ <741529838@qq.com>
2017-07-07 18:18:15 +0800

    Add files via upload

--- js/table.js
+++ js/table.js
@@ -9,9 +9,6 @@ document.getElementById("table-body").innerHTML = txt3;
 
 /*--------按钮函数------*/
 var model = document.getElementById("model");
-var name = document.getElementById("name").value;
-var content = document.getElementById("content").value;
-var value = document.getElementById("value").value;
 var yes = document.getElementById("yes");
 var cancel = document.getElementById("cancel");
 var yes2 = document.getElementById("yes2");
@@ -22,12 +19,42 @@ function addEventListener1(y){
 		model.style.display = "block";
 	};
 	yes.onclick = function(){
+		alert("yes");
+		var name = document.getElementById("name").value;
+		var content = document.getElementById("content").value;
+		var value = document.getElementById("value").value;
+		var id = y.id.charAt(4);
+		console.log(id);
+		var table_ago={
+			"id":id-1,
+			"name":rightJSON.table[id-1].name,
+			"content":rightJSON.table[id-1].content,
+			"value":rightJSON.table[id-1].value
+		}
+		var table_now = {
+			"id":id-1,
+			"name":name,
+			"content":content,
+			"value":value
+		}
+		/*rightJSON.table[id-1].name=name;
+		rightJSON.table[id-1].content=content;
+		rightJSON.table[id-1].value=value;*/
+		console.log(table_now);
+		console.log(table_ago);
+		//console.log(rightJSON.table[id-1].name);
+		rightJSON.table.splice(id-1,1,table_now);
+		console.log(JSON.stringify(rightJSON.table));
 		
+		model.style.display = "none";
+		mask.style.display = "none";
+		location.reload();
 	};
 	cancel.onclick = function(){
 		model.style.display = "none";
 		mask.style.display = "none";
 	};
+	return rightJSON;
 }
 
 function addEventListener2(z){
@@ -36,14 +63,25 @@ function addEventListener2(z){
 		model2.style.display = "block";
 	}
 	yes2.onclick = function(){
-		z.parentNode.parentNode.style.display = "none";
+		var id2 = z.id.charAt(6);
+		//z.parentNode.parentNode.style.display = "none";
+		delete rightJSON.table[id2];
+		var table_ago={
+			"id":id2-1,
+			"name":rightJSON.table[id2-1].name,
+			"content":rightJSON.table[id2-1].content,
+			"value":rightJSON.table[id2-1].value
+		}
+		rightJSON.table.splice(id2-1,1);
 		model2.style.display = "none";
 		mask.style.display = "none";
+		location.reload();
 	};
 	cancel2.onclick = function(){
 		model2.style.display = "none";
 		mask.style.display = "none";
 	};
+	return rightJSON;
 }
 for(var b=0; b<length4; b++){
 	var deleteNode = document.getElementById("delete"+b);
